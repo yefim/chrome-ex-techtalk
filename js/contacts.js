32bec3d36a319a67b41ae7843d2bfe9f68c8c059
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-09-04 19:12:13 -0400

    saving and rendering contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,7 +1,33 @@
 $(document).ready(function() {
+  // since localStorage only stores key/value pairs we will have to encode and decode JSON
+  function save(name, number) {
+    var contacts = {};
+    var data = localStorage["contacts"];
+    contacts = JSON.parse(data);
+    contacts[name] = number;
+    localStorage["contacts"] = JSON.stringify(contacts);
+  }
+
+  function addOne(name, number) {
+    console.log(name, number);
+  }
+
+  function render() {
+    var contacts = {};
+    var data = localStorage["contacts"];
+    contacts = JSON.parse(data);
+    for (name in contacts) {
+      addOne(name, contacts[name]);
+    }
+  }
+  
   $("#contact-form").submit(function(e) {
     e.preventDefault();
     var name = $("#name").val();
-    var num = $("#number").val();
+    var number = $("#number").val();
+    save(name, number);
+    addOne(name, number);
   });
+
+  render();
 });
