3057bde1b43b3a7150501350406f48a29d6fdde8
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-09-06 02:01:32 -0400

    Compiled JS templates

--- js/templates.js
+++ js/templates.js
@@ -15,4 +15,15 @@ templates['contact'] = template(function (Handlebars,depth0,helpers,partials,dat
   else { stack1 = depth0.number; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
   buffer += escapeExpression(stack1) + "</span>\n  <button class='button text'>Text</button>\n</div>\n";
   return buffer;});
+templates['text'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  helpers = helpers || Handlebars.helpers;
+  var buffer = "", stack1, foundHelper, functionType="function", escapeExpression=this.escapeExpression;
+
+
+  buffer += "<form id='text-form'>\n  <label for='text-body'>To ";
+  foundHelper = helpers.name;
+  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
+  else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
+  buffer += escapeExpression(stack1) + ":</label>\n  <input id='text-body' type='text'>\n  <input type='submit' value='Send'>\n</form>\n";
+  return buffer;});
 })();
